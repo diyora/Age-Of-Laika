15445a4326c4d78840211ee85261dbf586c1e8fe
diyora <mail@diyora.com>
2016-03-21 20:22:52 +0500

    hooked up the rest of the UI

--- Age Of Laika/ViewController.swift
+++ Age Of Laika/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+    }
 
 }
 
